d55d90db2e521b25a68cbb3f444d0c74bfb9d212
Daniel Sosa <danielsosa@Mac-DSP.local>
2024-01-25 16:52:58 -0600

    Initial Commit

---
+++ src/main/java/com/translate/translateapi/TranslateapiApplication.java
@@ -0,0 +1,13 @@
+package com.translate.translateapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TranslateapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TranslateapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/translate/translateapi/TranslateapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.translate.translateapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TranslateapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
